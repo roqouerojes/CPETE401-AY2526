dfa4f2bac9601c701d3b68ac01fbc6b9e4fc3483
roqouerojes <s1817278@usls.edu.ph>
2025-07-04 10:22:34 +0800

    Update act001.sql

--- act001.sql
+++ act001.sql
@@ -1,49 +1,59 @@
-
 create table authors(
-    id int(100), 
+    authors_id INT PRIMARY KEY AUTO_INCREMENT, 
     lname varchar(100), 
     fname varchar(100),
     biography text, 
-    dob date
+    date_of_birth date
 );
 
 create table publishers (
-    unique_id int(100), 
-    publisher varchar(100), 
-    country varchar(100)
+    unique_id INT PRIMARY KEY AUTO_INCREMENT, 
+    publisher_name varchar(100), 
+    country varchar(100) 
 );
 
 create table books (
-    bood_id int(100), 
-    title text, 
+    book_id INT PRIMARY KEY, 
+    booktitle text, 
     isbn int(100), 
     publication date, 
     price decimal (10,2) 
 );
 
 create table patients(
-    patient int(100), 
+    patient_id INT PRIMARY KEY AUTO_INCREMENT, 
     lname varchar(100), 
     fname varchar(100), 
     birth date, 
-    phone int(100), 
+    phone varchar(13), 
     consultation date
 );
 
 create table doctors(
-    doc_id int (100), 
+    doc_id INT PRIMARY KEY AUTO_INCREMENT, 
     doc_lname varchar(100), 
     doc_fname varchar(100), 
-    profession text, 
-    licensed date 
+    profession_specialty text, 
+    licensed int 
 );
 
 create table appointment(
-    appointment_id int(100), 
+    appointment_id INT PRIMARY KEY AUTO_INCREMENT, 
     appointment_and_start_time datetime, 
-    reason text, 
-    status enum ('scheduled', 'completed', 'canceled', 'no-show') 
+    reasonforthevisit text, 
+    status enum ('Scheduled', 'Completed', 'Canceled', 'No-Show') 
+);
+
+create table departments (
+    department_id INT PRIMARY KEY AUTO_INCREMENT,
+    department_name varchar(100),
+    deparment_loc varchar(100)
 );
 
 
-
+create table courses (
+    course_id INT PRIMARY KEY AUTO_INCREMENT,
+    course_code varchar (15),
+    course_title text,
+    credits int
+);
