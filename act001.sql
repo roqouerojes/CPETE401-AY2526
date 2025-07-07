9ab07a72821aa632516c430e52da925b89138475
roqouerojes <s1817278@usls.edu.ph>
2025-07-07 13:03:12 +0800

    Update act001.sql

--- act001.sql
+++ act001.sql
@@ -1,5 +1,5 @@
 create table authors(
-    authors_id int, 
+    authors_id INT, 
     lname varchar(100), 
     fname varchar(100),
     biography text, 
@@ -7,52 +7,52 @@ create table authors(
 );
 
 create table publishers (
-    unique_id int, 
+    publishers_id INT, 
     publisher_name varchar(100), 
-    country varchar(100) 
+    country varchar(100)
 );
 
 create table books (
-    book_id int, 
+    book_id INT, 
     booktitle text, 
     isbn int(100), 
     publication date, 
-    price decimal (10,2) 
+    price decimal (10,2)
 );
 
 create table patients(
-    patient_id int, 
+    patient_id INT, 
     lname varchar(100), 
     fname varchar(100), 
-    birth date, 
-    phone varchar(13), 
-    consultation date
+    date_of_birth date, 
+    phone_number varchar(13), 
+    consultation_date date
 );
 
 create table doctors(
-    doc_id int, 
+    doc_id INT, 
     doc_lname varchar(100), 
     doc_fname varchar(100), 
-    profession_specialty text, 
-    licensed int 
+    medical_specialty text, 
+    licensed_year int 
 );
 
 create table appointment(
-    appointment_id int, 
+    appointment_id INT, 
     appointment_and_start_time datetime, 
     reasonforthevisit text, 
-    status enum ('Scheduled', 'Completed', 'Canceled', 'No-Show') 
+    status enum ('Scheduled', 'Completed', 'Canceled', 'No-Show')
 );
 
 create table departments (
-    department_id int,
+    department_id INT,
     department_name varchar(100),
     deparment_loc varchar(100)
 );
 
 create table courses (
-    course_id int,
-    course_code varchar (15),
+    course_id INT,
+    course_code varchar (10),
     course_title text,
-    credits int
+    credits INT
 );
